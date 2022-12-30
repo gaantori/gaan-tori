2b4ec36096b8f569c9c1ad64cf24ed00bce41e60
Mehul Kundu <programmer.mehul@gmail.com>
2022-12-30 10:28:47 +0530

    mantine added

---
+++ components/Layout.tsx
@@ -0,0 +1,16 @@
+import { AppShell } from '@mantine/core'
+import React from 'react'
+
+type Props = {}
+
+function Layout({}: Props) {
+  return (
+    <div>
+      <AppShell>
+        Hello
+      </AppShell>
+    </div>
+  )
+}
+
+export default Layout
